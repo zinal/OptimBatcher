40f85f854c8b86eaf368171fbe02869e05a20d5b
Maksim Zinal <mzinal@ru.ibm.com>
2016-05-17 11:02:36 +0300

    Initial commit

---
+++ src/com/ibm/optim/batcher/Main.java
@@ -0,0 +1,19 @@
+/*
+ * IBM proprietary
+ */
+package com.ibm.optim.batcher;
+
+/**
+ *
+ * @author zinal_m
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
