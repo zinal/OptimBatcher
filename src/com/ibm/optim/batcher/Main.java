5fd432933de5a81d9970cab4bff8b161f1257ff1
Maksim Zinal <mzinal@ru.ibm.com>
2016-05-17 11:57:10 +0300

    Initial commit

--- src/com/ibm/optim/batcher/Main.java
+++ src/com/ibm/optim/batcher/Main.java
@@ -3,17 +3,71 @@
  */
 package com.ibm.optim.batcher;
 
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
 /**
  *
  * @author zinal_m
  */
 public class Main {
+    
+    private static final HashMap<String, Action> ACTIONS = new HashMap<>();
+    static {
+        ACTIONS.put("EXTRACT", new DoExtract());
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (args.length < 2) {
+            showHelpAndExit();
+        }
+        final Properties props = new Properties();
+        try {
+            final FileInputStream fis = new FileInputStream(args[0]);
+            try {
+                props.load(fis);
+            } finally {
+                fis.close();
+            }
+        } catch(Exception ex) {
+            System.err.println("ERROR: failed to load property file [" + args[0] + "]");
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        }
+        final Action action = ACTIONS.get(args[1].trim().toUpperCase());
+        if (action==null) {
+            showHelpAndExit();
+            throw new UnsupportedOperationException(); // UNREACHED
+        }
+        try {
+            action.run(args, props);
+        } catch(Exception ex) {
+            
+        }
+    }
+    
+    private static void showHelpAndExit() {
+        System.err.println("USAGE: java -jar OptimBatcher.jar OptimBatcher.properties ACTION ...\n"
+                + "\nValid actions and options:\n"
+                + "    EXTRACT data-source tables.txt\n");
+        System.exit(1);
+    }
+    
+    private static interface Action {
+        void run(String[] args, Properties props) throws Exception;
+    }
+    
+    private static class DoExtract implements Action {
+
+        @Override
+        public void run(String[] args, Properties props) throws Exception {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
     }
     
 }
